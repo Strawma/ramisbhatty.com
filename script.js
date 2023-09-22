4de5481c5ee4d983d59823fbf2cbce26b54931b0
Ramis <111586342+Strawma@users.noreply.github.com>
2023-09-22 22:36:28 +0100

    Ramis Toggle
    Added Ability to Devolve Ramis

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function EvolveRamis() {
 	document.getElementById("ramisImage").src = "goober.png";
+	document.getElementById("ramisImage").onclick = "DevolveRamis()"; 
+}
+
+function DevolveRamis() {
+	document.getElementById("ramisImage").src = "ramis-real.jpg";
+	document.getElementById("ramisImage").onclick = "EvolveRamis()"; 
 }
