7bf899f605af5977af17c5383c1196eb9b3be5cc
Ramis <111586342+Strawma@users.noreply.github.com>
2023-09-24 23:24:41 +0100

    RAINBOW BACKGROUND
    MWUHAHAHAHA

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ function DevolveRamis() {
 	var audio = new Audio('splat.mp3');
 	audio.play();
 }
+
+function pause (milliseconds) {
+	return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+function RColValue() {
+	return Math.floor(Math.random() * 256);
+}
+
+function GetDifference(start, end, interval) {
+	return Math.floor((end - start) / interval);
+}
+
+async function RandomBackground(currentColor) {
+	const frames = 60;
+	var newColor = [RColValue(), RColValue(), RColValue()];
+	var d = [0,0,0];
+	for (let i = 0; i < 3; i++) {
+		d[i] = GetDifference(currentColor[i], newColor[i], frames);
+	}
+	for (let j = 0; j < frames; j++) {
+		await pause(Math.floor(1000 / frames));
+		for (let k = 0; k < 3; k++) {
+			currentColor[k] += d[k];
+		}
+		document.body.style.backgroundColor="rgb("+currentColor[0]+","+currentColor[1]+","+currentColor[2]+")";
+	} 
+	RandomBackground(currentColor);
+}
+
+function InitialiseBackground() {
+	var startColor = [255, 255, 255];
+	document.body.style.backgroundColor="rgb(200,200,200)";
+	RandomBackground(startColor);
+}
