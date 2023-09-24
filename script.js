6af46edd0af38aa0614a03d65975232415454024
Ramis <111586342+Strawma@users.noreply.github.com>
2023-09-24 21:22:28 +0100

    Text Changes
    text now changes when you click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function EvolveRamis() {
 	document.getElementById("ramisImage").src = "goober.png";
 	document.getElementById("ramisImage").onclick = DevolveRamis; 
+	document.getElementById("thisIsX").innerHTML = "this is a Goober";
 	var audio = new Audio('splat.mp3');
 	audio.play();
 }
@@ -8,6 +9,7 @@ function EvolveRamis() {
 function DevolveRamis() {
 	document.getElementById("ramisImage").src = "ramis-real.jpg";
 	document.getElementById("ramisImage").onclick = EvolveRamis; 
+	document.getElementById("thisIsX").innerHTML = "this is a Ramis Bhatty";
 	var audio = new Audio('splat.mp3');
 	audio.play();
 }
