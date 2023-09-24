eadb22e619b93d5d7af4a65fc03359c8fa393694
Ramis <111586342+Strawma@users.noreply.github.com>
2023-09-24 21:13:33 +0100

    splat fix
    plays splat without html element

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function EvolveRamis() {
 	document.getElementById("ramisImage").src = "goober.png";
 	document.getElementById("ramisImage").onclick = DevolveRamis; 
-	document.getElementById("splat").play();
+	var audio = new Audio('splat.mp3');
+	audio.play();
 }
 
 function DevolveRamis() {
 	document.getElementById("ramisImage").src = "ramis-real.jpg";
 	document.getElementById("ramisImage").onclick = EvolveRamis; 
-	document.getElementById("splat").play();
+	var audio = new Audio('splat.mp3');
+	audio.play();
 }
