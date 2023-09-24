95275ac81b1159e5bf3281f9cc258414d98c9e77
Ramis <111586342+Strawma@users.noreply.github.com>
2023-09-24 21:00:09 +0100

    update script.js
    now plays a sploink sound

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function EvolveRamis() {
 	document.getElementById("ramisImage").src = "goober.png";
 	document.getElementById("ramisImage").onclick = DevolveRamis; 
+	document.getElementById("splat.mp3").play();
 }
 
 function DevolveRamis() {
 	document.getElementById("ramisImage").src = "ramis-real.jpg";
 	document.getElementById("ramisImage").onclick = EvolveRamis; 
+	document.getElementById("splat.mp3").play();
 }
