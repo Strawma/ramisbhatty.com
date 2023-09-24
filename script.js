dddadf31dc78c3ac25f77b9daa91bc338b6ce9a4
Ramis <111586342+Strawma@users.noreply.github.com>
2023-09-24 21:09:28 +0100

    added splat
    added splat sound as element

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function EvolveRamis() {
 	document.getElementById("ramisImage").src = "goober.png";
 	document.getElementById("ramisImage").onclick = DevolveRamis; 
-	document.getElementById("splat.mp3").play();
+	document.getElementById("splat").play();
 }
 
 function DevolveRamis() {
 	document.getElementById("ramisImage").src = "ramis-real.jpg";
 	document.getElementById("ramisImage").onclick = EvolveRamis; 
-	document.getElementById("splat.mp3").play();
+	document.getElementById("splat").play();
 }
