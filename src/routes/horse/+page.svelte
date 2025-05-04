90364f3e18c0ddcffdede1214c4fd55383fd575a
Ramis <dev@ramisbhatty.com>
2025-05-05 00:03:38 +0100

    tried adding a horse

---
+++ src/routes/horse/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+
+<div class="top-bar fixed top-0 left-0 w-full h-16 bg-black bg-opacity-30 backdrop-blur-sm z-50 flex items-center justify-between px-4 shadow-md">
+  <div class="logo font-[Comic_Sans_MS] text-xl font-bold">You Should Not Be Here</div>
+</div>
+
+<main class="flex flex-col items-center justify-center h-screen bg-black">
+  <h1 class="font-[Comic_Sans_MS]">Leave</h1>
+</main>
+
