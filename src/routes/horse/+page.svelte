9882aad1a2b8330fc088b3ae6e35f4471e63fc8e
Ramis <dev@ramisbhatty.com>
2025-06-01 15:03:31 +0100

    remove blazor stuff for now

--- src/routes/horse/+page.svelte
+++ src/routes/horse/+page.svelte
@@ -3,11 +3,5 @@
 </script>
 
 <div class="w-full h-screen">
-	<iframe
-		src="/horseracetests/index.html"
-		title="Horse Race Blazor Game"
-		class="w-full h-full border-none"
-	>
-		Your browser does not support iframes.
-	</iframe>
+
 </div>
