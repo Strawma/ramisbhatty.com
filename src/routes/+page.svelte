66e02486846c4819a1573790ca222e29ce77d94c
Ramis <dev@ramisbhatty.com>
2025-10-14 16:03:18 +0100

    redoing main page from scratch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,9 @@
 <script lang="ts">
-  import RamisSrc from '$lib/assets/images/ramis.jpg';
-  import GooberSrc from '$lib/assets/images/goober.jpg';
-  import SplatSource from '$lib/assets/sounds/splat.mp3';
-  import Bouncer from "./Bouncer.svelte";
 
-  const bouncerSrcs = [RamisSrc, GooberSrc];
-  const bouncerNames = ["Ramis", "Goober"];
-  let bouncerIndex = 0;
-  $: bouncerName = bouncerNames[bouncerIndex];
-  $: bouncerSrc = bouncerSrcs[bouncerIndex];
-
-  function handleClick() {
-    bouncerIndex = 1 - bouncerIndex;
-    //play splat noise
-    let splatSound = new Audio(SplatSource);
-    splatSound.play();
-  }
 </script>
 
-<div class="top-bar fixed top-0 left-0 w-full h-16 bg-white bg-opacity-30 backdrop-blur-sm z-50 flex items-center justify-between px-4 shadow-md">
-  <div class="logo font-[Comic_Sans_MS] text-xl font-bold">Bouncy Friendos</div>
-  <nav class="flex gap-4">
-    <a href="/" class="font-[Comic_Sans_MS] hover:underline">Home</a>
-  </nav>
-</div>
 
-<main class="flex flex-col items-center justify-center h-screen">
-  <h1 class="font-[Comic_Sans_MS]">This is a {bouncerName}</h1>
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-  <Bouncer name={bouncerName} src={bouncerSrc} on:click={handleClick} />
-</main>
 
 <style>
-  @import '$lib/RainbowBody.css';
+
 </style>
