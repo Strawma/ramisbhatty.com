2544a7d4bf12016f4b04440e8521f512093a6e77
Ramis <dev@ramisbhatty.com>
2025-10-14 21:50:17 +0100

    make bouncers run like ass

--- src/routes/Bouncer.svelte
+++ src/routes/Bouncer.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import bounceSrc from '$lib/assets/sounds/blip.mp3';
 
   const maxWidth = 150;
   const maxHeight = 150;
   let width: number;
   let height: number;
-  const minSpeed = 2;
-  const maxSpeed = 15;
+  const minSpeed = 100;
+  const maxSpeed = 400;
   export let name = "Bouncer";
   export let src = "";
+	export let bounceSrc = null;
 	export let initialX = 0;
 	export let initialY = 0;
+	export let fps = 8;
 
 	let x = initialX;
 	let y = initialY;
@@ -19,6 +20,7 @@
   let dy = Math.floor(Math.random() * maxSpeed) + minSpeed;
   let animationFrame: number;
   let lastTime = performance.now();
+	let totalTime = 0;
 
   function adjustSize() {
     width = Math.min(maxWidth, window.innerWidth);
@@ -35,47 +37,52 @@
     // adjust size of bouncer if window size changes
     adjustSize();
 
-    // Calculate the time delta
-    const deltaTime = (timestamp - lastTime) / 16.6667;
-    lastTime = timestamp;
+    // check if total of 83ms has passed since last frame
+		const deltaTime = timestamp - lastTime;
+		lastTime = timestamp;
+		totalTime += deltaTime;
 
-    // Get the container's boundaries
-    const containerRect = document.documentElement.getBoundingClientRect();
-    let leftBoundary = containerRect.left;
-    let topBoundary = containerRect.top;
-    let rightBoundary = containerRect.right;
-    let bottomBoundary = containerRect.bottom;
+		// only run at 12fps
+		if (totalTime >= 1000 / fps) {
+			totalTime -= 1000 / fps;
+			// Get the container's boundaries
+			const containerRect = document.documentElement.getBoundingClientRect();
+			let leftBoundary = containerRect.left;
+			let topBoundary = containerRect.top;
+			let rightBoundary = containerRect.right;
+			let bottomBoundary = containerRect.bottom;
 
-    // Update the position
-    x += dx * deltaTime;
-    y += dy * deltaTime;
+			// Update the position
+			x += dx / fps;
+			y += dy / fps;
 
-    let bounced = false;
-    // Check for collisions with the container's boundaries
-    if (x + window.scrollX <= leftBoundary) {
-      dx = Math.abs(dx);
-      x = 0;
-      bounced = true;
-    } else if (x + width + window.scrollX >= rightBoundary) {
-      dx = -Math.abs(dx);
-      x = window.innerWidth - width;
-      bounced = true;
-    }
+			let bounced = false;
+			// Check for collisions with the container's boundaries
+			if (x + window.scrollX <= leftBoundary) {
+				dx = Math.abs(dx);
+				x = 0;
+				bounced = true;
+			} else if (x + width + window.scrollX >= rightBoundary) {
+				dx = -Math.abs(dx);
+				x = window.innerWidth - width;
+				bounced = true;
+			}
 
-    if (y + window.scrollY <= topBoundary) {
-      dy = Math.abs(dy);
-      y = 0;
-      bounced = true;
-    } else if (y + height + window.scrollY >= bottomBoundary) {
-      dy = -Math.abs(dy);
-      y = window.innerHeight - height;
-      bounced = true;
-    }
+			if (y + window.scrollY <= topBoundary) {
+				dy = Math.abs(dy);
+				y = 0;
+				bounced = true;
+			} else if (y + height + window.scrollY >= bottomBoundary) {
+				dy = -Math.abs(dy);
+				y = window.innerHeight - height;
+				bounced = true;
+			}
 
-    // play bounce sound if bouncer hits a wall
-    if (bounced) {
-      bounceSound();
-    }
+			// play bounce sound if bouncer hits a wall
+			if (bounced) {
+				bounceSound();
+			}
+		}
 
     // Request the next frame
     animationFrame = requestAnimationFrame(moveBouncer);
