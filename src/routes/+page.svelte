a4741c8d5a96b706b2a23e25100d5693437cd041
Ramis <dev@ramisbhatty.com>
2025-10-15 16:28:59 +0100

    added pumping lemma info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -166,7 +166,9 @@
 			<div class="md:col-span-2">
 				<div class="bg-white border-4 border-black p-4 text-center" style="box-shadow: 4px 4px 0 #000;">
 					<p class="text-sm mb-2">-- AD SPACE --</p>
-					<img src={AoOniSrc} alt="Ad" class="mx-auto w-64 h-48 object-contain" />
+					<a href="https://cse103-notes.readthedocs.io/en/latest/pumpinglemma.html">
+						<img src={AoOniSrc} alt="Ad" class="mx-auto w-64 h-48 object-contain" />
+					</a>
 					<p class="text-xs mt-2">Your Ad Here!</p>
 					<p class="text-xs mt-2 break-all"><a href="mailto:advertising@ramisbhatty.com" class="text-blue-600 underline hover:text-red-600">advertising@ramisbhatty.com</a></p>
 				</div>
