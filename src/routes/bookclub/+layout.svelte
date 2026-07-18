bebb0837a1b115b9a1349291959f8f05f723fd66
Ramis <dev@ramisbhatty.com>
2026-07-18 14:00:21 +0100

    added bookclub favicon

--- src/routes/bookclub/+layout.svelte
+++ src/routes/bookclub/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.ico" />
+	<link rel="icon" href="/bookclub/favicon.svg" type="image/svg+xml" />
 </svelte:head>
 
 {@render children()}
