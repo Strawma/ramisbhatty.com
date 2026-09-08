ec635d29f57a907bfd2f895dea76a24e4113bd28
Ramis <dev@ramisbhatty.com>
2026-09-08 18:20:59 +0100

    stop crawlers from awknowledging dev page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="canonical" href="https://ramisbhatty.com{page.url.pathname}" />
+</svelte:head>
 
 {@render children()}
